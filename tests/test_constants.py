442530e76aefc7ca7eb8fd91fe03cce94e4195dd
patrick <patrickwuke@163.com>
2024-04-05 18:08:45 +0800

    clean up all

--- tests/test_constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from backup.qpyone.constants import get_base_dir, BASE_DIR
-
-
-def test_get_base_dir():
-    print(get_base_dir())
-    print(BASE_DIR)
-    print(os.path.join(BASE_DIR, "templates"))
